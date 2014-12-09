7a1430c03bc38c10452c7ee56551d2ebacfd2ab0
diwasblack <diwasblack@gmail.com>
2014-12-09 19:07:32 +0545

    Removed previous code for login and signup

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,69 +10,11 @@ from mainapp.models import Media, Food, Dish, CookInfo, Order
 
 
 # Create your views here.
-class LoginForm(forms.Form):
-    username=forms.CharField(max_length=30,widget=forms.TextInput(attrs={'class':'form-control'}))
-    password=forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control'}))
-
 def login(request):
-    if(request.method== 'POST'):
-        form=LoginForm(request.POST)
-        if(form.is_valid()):
-            user=auth.authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-            if user is not None:
-                if user.is_active:
-                    auth.login(request,user)
-                    return redirect('mainapp:home')
-                else:
-                    # Disabled account
-                    pass
-            else:
-                new_form=LoginForm()
-                return render(request, 'mainapp/login.html',{'form':form,'message':'Wrong Username or password'})
-    else:
-        form=LoginForm()
-        return render(request, 'mainapp/login.html',{'form':form})
+    return render(request, 'mainapp/login.html')
 
-class SignupForm(forms.ModelForm):
-    class Meta:
-        model= auth.models.User
-        fields= ['username','email','first_name','last_name','password']
-        widgets = {
-            'username': forms.TextInput(attrs={'class':'form-control'}),
-            'email': forms.TextInput(attrs={'class':'form-control'}),
-            'first_name': forms.TextInput(attrs={'class':'form-control'}),
-            'last_name': forms.TextInput(attrs={'class':'form-control'}),
-            'password': forms.PasswordInput(attrs={'class':'form-control'}),
-        }
-
-    repassword=forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 def signup(request):
-    if (request.method== 'POST'):
-        form=SignupForm(request.POST)
-        if (form.is_valid()):
-            username=form.cleaned_data['username']
-            email=form.cleaned_data['email']
-            password=form.cleaned_data['password']
-            repassword=form.cleaned_data['repassword']
-            first_name=form.cleaned_data['first_name']
-            last_name=form.cleaned_data['last_name']
-
-            if(password!=repassword):
-                return render(request,'mainapp/signup.html',{'form':form})
-
-            user = auth.models.User.objects.create_user(username, email, password)
-            user.first_name=first_name
-            user.last_name=last_name
-            user.save()
-
-            new_form=SignupForm()
-            user=auth.authenticate(username=username, password=password)
-            return redirect('mainapp:home')
-
-        else:
-            return render(request,'mainapp/signup.html',{'form':form,'message':'Invalid form'})
-    form=SignupForm()
     return render(request,'mainapp/signup.html',{'form':form})
 
 def index(request):
